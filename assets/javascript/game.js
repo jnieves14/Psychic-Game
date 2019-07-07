5118bfef2e291db1c0bf94e77b65219c31df3abd
Jannah June Nieves <jannahjune@gmail.com>
2019-07-06 19:03:54 -0700

    increased # of guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 var options = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var wins = 0;
 var losses = 0;
-var guessesLeft = 8;
+var guessesLeft = 9;
 var guessesSoFar = [];
 var computerGuess;
 
@@ -22,7 +22,7 @@ alert("You chose " + userGuess);
   
     if (userGuess===computerGuess) {
     wins++;
-    guessesLeft= 8;
+    guessesLeft= 9;
     guessesSoFar = [];
     alert("You've guessed correctly!");
     }
